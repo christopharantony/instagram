1c2494550d923e86ae004fb6ac9de21322a00154
Christophar Antony <christopharantony@gmail.com>
2023-03-19 02:41:20 +0530

    added some assets

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import Assets from '../views/Assets';
 import Dashboard from '../views/Dashboard';
 import NotFound from '../views/NotFound';
 import Components from '../views/SampleComponent';
@@ -12,6 +13,10 @@ export const routes = [
   {
     path: '/components',
     Component: Components,
+  },
+  {
+    path: '/assets',
+    Component: Assets,
   },
   {
     path: '/404',
---
+++ src/views/Assets/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+import React from 'react';
+
+function Assets() {
+  console.log('assets');
+  return (
+    <div>
+      <div> Assets </div>
+      <div>hai</div>
+    </div>
+  );
+}
+
+export default Assets;
